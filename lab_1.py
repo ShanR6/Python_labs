59add06b334d0c6995d63b82987a7ab85eebe49d
ShanR6 <72306836+ShanR6@users.noreply.github.com>
2023-11-19 15:20:50 +0200

    Delete lab_1.py

--- lab_1.py
+++
@@ -1,9 +0,0 @@
-print('Введите числа:')
-a, b, c = float(input()), float(input()), float(input())
-
-if a == b == c:
-    print(3)
-elif a == b or a == c or b == c:
-    print(2)
-else:
-    print(0)
